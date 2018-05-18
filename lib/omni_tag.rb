eeef1f27b4c210f729df79e11c09f98e64a103a5
Andersen Fan <as181920@gmail.com>
2018-05-18 22:31:12 +0800

    Refactor tag handling and Add query support

--- lib/omni_tag.rb
+++ lib/omni_tag.rb
@@ -9,44 +9,48 @@ module OmniTag
   end
 
   class_methods do
+    def tagged_with(name, options={}) # TODO: support match_all, any, exclude options with chainable
+      context_scope = options.has_key?(:context) ? joins(:tags).merge(::OmniTag::Tagging.by_context(options[:context])) : joins(:tags)
+      context_scope.merge(::OmniTag::Tag.where(name: Array(name)))
+    end
   end
 
   def tag_names(options={})
-    if options.has_key?(:context)
-      tags.by_context(options[:context]).map(&:name).uniq
-    else
-      tags.map(&:name).uniq
-    end
+    context_scope = options.has_key?(:context) ?  tags.by_context(options[:context]) : tags
+    context_scope.map(&:name).uniq
   end
-  alias_method :get_tag_names, :tag_names
 
-  def add_tag_names(names, options={})
+  def add_tags(names, options={})
     Array[options[:context]].each do |context|
-      Array(names).compact.uniq.each { |name| taggings.find_or_create_by(tag: ::OmniTag::Tag.find_or_create_by(name: name), context: context) }
+      Array(names).compact.uniq.each { |name| taggings.find_or_create_by(tag: tag_instance(name), context: context) }
     end
 
     self.tag_names(options)
   end
-  alias_method :add_tag_name, :add_tag_names
+  alias_method :add_tag, :add_tags
 
-  def remove_tag_names(names, options={})
-    if options.has_key?(:context)
-      taggings.where(context: Array[options[:context]]).where(tag: ::OmniTag::Tag.where(name: names)).destroy_all
-    else
-      taggings.where(tag: ::OmniTag::Tag.where(name: names)).destroy_all
-    end
+  def remove_tags(names, options={})
+    context_scope = options.has_key?(:context) ? taggings.where(context: Array[options[:context]]) : taggings
+    context_scope.where(tag: tag_instances(names)).destroy_all
 
     self.tag_names(options)
   end
-  alias_method :remove_tag_name, :remove_tag_names
+  alias_method :remove_tag, :remove_tags
 
-  def set_tag_names(names, options={})
-    # self.tags = Array(names).compact.uniq.map { |name| ::OmniTag::Tag.by_context(options[:context]).find_or_create_by(name: name) } # Not trigger after_destroy for taggings
-
+  def set_tags(names, options={})
     target_names = Array(names).compact.uniq
-    remove_tag_names((tag_names(options) - target_names), options)
-    Array[options[:context]].each { |cxt| add_tag_names((target_names - tag_names(context: cxt)), context: cxt) }
+    remove_tags((tag_names(options) - target_names), options)
+    Array[options[:context]].each { |cxt| add_tags((target_names - tag_names(context: cxt)), context: cxt) }
 
     self.tag_names(options)
   end
+
+  private
+    def tag_instance(name)
+      ::OmniTag::Tag.find_or_create_by(name: name)
+    end
+
+    def tag_instances(names)
+      ::OmniTag::Tag.where(name: Array(names))
+    end
 end
