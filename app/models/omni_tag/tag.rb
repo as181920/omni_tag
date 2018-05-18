6b13abe0ac4a68e3131174306f4b1539cc6528a0
Andersen Fan <as181920@gmail.com>
2018-05-18 18:20:57 +0800

    Implement taggable concern module

--- app/models/omni_tag/tag.rb
+++ app/models/omni_tag/tag.rb
@@ -2,6 +2,8 @@ module OmniTag
   class Tag < ApplicationRecord
     has_many :taggings, dependent: :destroy, class_name: "::OmniTag::Tagging"
 
+    scope :by_context, ->(context) { (defined?(proxy_association) ? all : joins(:taggings)).merge(::OmniTag::Tagging.by_context(context)).distinct }
+
     validates :name, presence: true, uniqueness: true
 
     def to_s
--- lib/omni_tag.rb
+++ lib/omni_tag.rb
@@ -3,6 +3,50 @@ require "omni_tag/engine"
 module OmniTag
   extend ActiveSupport::Concern
 
+  included do
+    has_many :taggings, as: :taggable, dependent: :destroy, class_name: "::OmniTag::Tagging"
+    has_many :tags, through: :taggings, class_name: "::OmniTag::Tag"
+  end
+
   class_methods do
+  end
+
+  def tag_names(options={})
+    if options.has_key?(:context)
+      tags.by_context(options[:context]).map(&:name).uniq
+    else
+      tags.map(&:name).uniq
+    end
+  end
+  alias_method :get_tag_names, :tag_names
+
+  def add_tag_names(names, options={})
+    Array[options[:context]].each do |context|
+      Array(names).compact.uniq.each { |name| taggings.find_or_create_by(tag: ::OmniTag::Tag.find_or_create_by(name: name), context: context) }
+    end
+
+    self.tag_names(options)
+  end
+  alias_method :add_tag_name, :add_tag_names
+
+  def remove_tag_names(names, options={})
+    if options.has_key?(:context)
+      taggings.where(context: Array[options[:context]]).where(tag: ::OmniTag::Tag.where(name: names)).destroy_all
+    else
+      taggings.where(tag: ::OmniTag::Tag.where(name: names)).destroy_all
+    end
+
+    self.tag_names(options)
+  end
+  alias_method :remove_tag_name, :remove_tag_names
+
+  def set_tag_names(names, options={})
+    # self.tags = Array(names).compact.uniq.map { |name| ::OmniTag::Tag.by_context(options[:context]).find_or_create_by(name: name) } # Not trigger after_destroy for taggings
+
+    Array(names).tap(&:compact!).tap(&:uniq!)
+    remove_tag_names((tag_names(options) - names), options)
+    Array[options[:context]].each { |cxt| add_tag_names((names - tag_names(context: cxt)), context: cxt) }
+
+    self.tag_names(options)
   end
 end
